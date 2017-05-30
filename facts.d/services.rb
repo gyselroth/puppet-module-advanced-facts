851a8854efa6230e677c29cc7dd6c8ba870fc045
Raffael Sahli <sahli@gyselroth.com>
2017-05-30 16:11:23 +0200

    fixed systemd listing

--- facts.d/services.rb
+++ facts.d/services.rb
@@ -3,7 +3,7 @@ Facter.add("services") do
     systemd = Facter::Util::Resolution.exec("/usr/bin/dpkg-query --show -f '${status}' systemd | cut -d ' ' -f1")
   
     if systemd == "install"
-        list = Facter::Util::Resolution.exec("systemctl list-units   | grep '.service' | cut -d ' ' -f1 | cut -d '.' -f1")
+        list = Facter::Util::Resolution.exec("systemctl list-units --plain | sed 's/^[ \t]*//' | grep '.service' | cut -d ' ' -f1 | cut -d '.' -f1")
     else
         list = Facter::Util::Resolution.exec("ls /etc/init.d")
     end
