ecc7a3e91ef11abf9a9ed854df6d03d335a5cc6a
Raffael Sahli <sahli@gyselroth.com>
2017-05-30 14:55:04 +0200

    added systemd services if installed

--- facts.d/services.rb
+++ facts.d/services.rb
@@ -1,8 +1,14 @@
 Facter.add("services") do
     services = []
-    initd = Facter::Util::Resolution.exec("ls /etc/init.d")
-        
-    initd.each_line do |line|
+    systemd = Facter::Util::Resolution.exec("/usr/bin/dpkg-query --show -f '${status}' systemd | cut -d ' ' -f1")
+  
+    if systemd == "install"
+        list = Facter::Util::Resolution.exec("systemctl list-units   | grep '.service' | cut -d ' ' -f1 | cut -d '.' -f1")
+    else
+        list = Facter::Util::Resolution.exec("ls /etc/init.d")
+    end
+    
+    list.each_line do |line|
         services.push(line.strip().split().join(" "))
     end
         
