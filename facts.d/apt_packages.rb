db6fa9481b1cd40963ec373f1a64490b61f6332e
Raffael Sahli <sahli@gyselroth.com>
2018-06-01 12:24:47 +0200

    include half-configured/unpacked packages

--- facts.d/apt_packages.rb
+++ facts.d/apt_packages.rb
@@ -1,14 +1,14 @@
 Facter.add("packages") do
-    confine :kernel => :linux
+    confine :kernel => 'Linux'
+ setcode do
     list = {}
-    packs = Facter::Util::Resolution.exec("/usr/bin/dpkg-query --show --showformat '${Package};${Version}\n'")
+    packs = Facter::Util::Resolution.exec("/usr/bin/dpkg-query --show --showformat '${Package};${Version};${Status}\n' | grep 'installed\\|half-configured\\|unpacked'")
 
     packs.each_line do |package|
         split = package.split(';')
         list[split[0]] = split[1].split("\n")[0]
     end
 
-    setcode do
       list
     end
 end
